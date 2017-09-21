ddb82def4db191a6955a1ec4262028de34d6e551
Ayda <30689760+Ayda5@users.noreply.github.com>
2017-09-21 15:40:41 +0200

    Add files via upload

---
+++ Measures converter.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Author: Ajda Pavčič
+
+print "This program converts distances measured in kilometers to miles."
+print "To start the Unit Converter please type the number next to the conversion you would like to perform."
+
+
+while True:
+    kilometers = int(raw_input ("Enter value (numbers only) in kilometers").replace(',',''))
+    # conversion factor mile_value = 0.621371
+    # calculate miles
+    miles = (kilometers * 0.621371)
+    print('%s kilometers is equal to %s miles' %(kilometers,miles))
+
+else:
+    print ("This value is invalid please make sure that you entered it correctly.")
