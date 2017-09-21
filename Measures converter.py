c5d7cb9ac61106841bbe7b26d8613093f44b4637
Ayda <30689760+Ayda5@users.noreply.github.com>
2017-09-21 15:45:57 +0200

    Update Measures converter.py

--- Measures converter.py
+++ Measures converter.py
@@ -9,8 +9,8 @@ while True:
     kilometers = int(raw_input ("Enter value (numbers only) in kilometers").replace(',',''))
     # conversion factor mile_value = 0.621371
     # calculate miles
-    miles = (kilometers * 0.621371)
-    print('%s kilometers is equal to %s miles' %(kilometers,miles))
+    miles = kilometers * 0.621371
+    print '%s kilometers is equal to %s miles' %(kilometers,miles)
 
 else:
-    print ("This value is invalid please make sure that you entered it correctly.")
+    print "This value is invalid please make sure that you entered it correctly."
